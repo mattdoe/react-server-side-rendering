4811d470a6502431b4d19296dc033ee223b21be5
Matt Doe <doe.matthew@gmail.com>
2018-11-14 18:46:33 -0500

    Serve Auto container

--- src/server/server.js
+++ src/server/server.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import express from 'express';
+
+import Auto from '../container/Auto';
+
+const server = express();
+
+server.get('/auto', (req, res) => {
+    const autoString = renderToString(<Auto />);
+
+    res.send(autoString);
+});
